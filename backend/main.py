ba6dd8e05bd9272591388a358e9fd461696dcb22
Abhinav <22cs035@jssaten.ac.in>
2025-06-28 21:36:00 +0530

    dploy_fix 1

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
+import os
+
 from .routes import chat
 app = FastAPI()
 
 
 app.include_router(chat.router , prefix='/api')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
